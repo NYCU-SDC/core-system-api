b0904a017da8ae165917f37be9d5958419f9b6ad
Elvis Mao <info@elvismao.com>
2026-01-27 15:45:11 +0800

    fix: Re-export specific models

--- orval.config.js
+++ orval.config.js
@@ -3,9 +3,10 @@ module.exports = {
 	api: {
 		input: "./tsp-output/schema/openapi.1.0.0.yaml",
 		output: {
-			target: "./packages/sdk/src/index.ts",
-			schemas: "./packages/sdk/src/model",
+			target: "./packages/sdk/src/generated/index.ts",
+			schemas: "./packages/sdk/src/generated/model",
 			client: "fetch",
+			enumGenerationType: "enum",
 			clean: true,
 			prettier: true
 		}
---
+++ packages/sdk/src/index.ts
@@ -0,0 +1,4 @@
+export * from './generated'
+
+// Re-export specific models
+export { AuthOAuthProviders } from './generated/model/authOAuthProviders'
